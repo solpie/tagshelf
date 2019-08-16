3136e8a9e11d64e58627f26d0bfa3ec3657d0ffc
solpie <solpie.net@gmail.com>
2019-08-16 11:37:27 +0800

    setup and drag out test

---
+++ src/components/Shelf.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-layout>
+      <a href="#" id="drag">NKI Drag out</a>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+const ipcRenderer = require('electron').ipcRenderer;
+export default {
+  data: () => ({}),
+  mounted: () => {
+    document.getElementById("drag").ondragstart = event => {
+      event.preventDefault();
+      ipcRenderer.send("ondragstart", "C:\\test.nki");
+    };
+  }
+};
+</script>
